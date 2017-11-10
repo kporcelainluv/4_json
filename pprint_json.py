e29fdf9a9377f9f96ec320a1f719dbbdda53735f
kporcelain <zhukovaksusha@gmail.com>
2017-11-10 13:09:13 +0300

    first program push

--- pprint_json.py
+++ pprint_json.py
@@ -1,13 +1,20 @@
 import json
+import sys
 
 
-def load_data(filepath):
-    pass
+def load_data():
+    with open(filename, "r") as input_file:
+        json_file = input_file.read().strip()
+    return json_file
 
 
-def pretty_print_json(data):
-    pass
+def pretty_print_json(data_from_file):
+    load_data_from_file = json.loads(data_from_file)
+    json_format_file = json.dumps(load_data_from_file, indent=5, sort_keys=True, ensure_ascii=False)
+    return json_format_file
 
 
 if __name__ == '__main__':
-    pass
+    if len(sys.argv) > 1:
+        filename = sys.argv[1]
+        print(pretty_print_json(load_data()))
