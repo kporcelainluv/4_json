dedeb205b975d8721c07940b66e33e3cf8c55e20
kporcelain <zhukovaksusha@gmail.com>
2017-11-17 12:46:25 +0300

    got rid of global vars

--- pprint_json.py
+++ pprint_json.py
@@ -1,20 +1,12 @@
 import json
 import sys
 
-
-def load_data():
-    with open(filename, "r") as input_file:
-        json_file = input_file.read().strip()
-    return json_file
-
-
-def pretty_print_json(data_from_file):
-    load_data_from_file = json.loads(data_from_file)
-    json_format_file = json.dumps(load_data_from_file, indent=5, sort_keys=True, ensure_ascii=False)
-    return json_format_file
+def convert_file_to_pretty(data_from_file):
+    prettyfied_file = json.dumps(json.loads(data_from_file), indent=5, sort_keys=True, ensure_ascii=False)
+    return prettyfied_file
 
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
-        filename = sys.argv[1]
-        print(pretty_print_json(load_data()))
+        with open(sys.argv[1], "r") as input_file:
+            print(convert_file_to_pretty(input_file.read()))
