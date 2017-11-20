cba1a761d790a6f57a22212f2935d7c3afa70a87
kporcelain <zhukovaksusha@gmail.com>
2017-11-20 13:12:39 +0300

    changed var name

--- pprint_json.py
+++ pprint_json.py
@@ -1,12 +1,14 @@
 import json
 import sys
 
-def convert_file_to_pretty(data_from_file):
-    prettyfied_file = json.dumps(json.loads(data_from_file), indent=5, sort_keys=True, ensure_ascii=False)
+
+def prettify_input_file(data_from_file):
+    prettyfied_file = json.dumps(json.loads(data_from_file),
+                                 indent=5, sort_keys=True, ensure_ascii=False)
     return prettyfied_file
 
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
         with open(sys.argv[1], "r") as input_file:
-            print(convert_file_to_pretty(input_file.read()))
+            print(prettify_input_file(input_file.read()))
